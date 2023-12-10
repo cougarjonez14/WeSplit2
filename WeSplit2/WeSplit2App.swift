47ed845c4eca73c3f04988de6457346181e9698f
stationAstral <cougarjonez@gmail.com>
2023-12-10 06:13:27 -0800

    Initial Commit

---
+++ WeSplit2/WeSplit2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit2App.swift
+//  WeSplit2
+//
+//  Created by stationAstral on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
